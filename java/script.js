82fc95942c773e0bf8af951bff4bcedb41d3cece
Nickolas-Garciaa <nickolas.avelino@etec.sp.gov.br>
2023-03-19 12:23:28 -0300

    Update script.js

---
+++ java/script.js
@@ -0,0 +1,38 @@
+Function zelda (){
+    let zeldaimg = document.querySelector("img#imagem")
+    let num = math.trunc("math.random"()*10) ;
+
+    if (num == 0) {
+        zeldaimg.setAttribute ( "src", "img/zelda.jpg")
+    }
+    else if (num == 1) {
+        zeldaimg.setAttribute ( "src", "img/zelda1.jpg")
+    }
+    else if (num == 2) {
+        zeldaimg.setAttribute ( "src", "img/zelda2.jpg")
+    } 
+    else if (num == 3) {
+        zeldaimg.setAttribute ( "src", "img/zelda3.jpg")
+    } 
+    else if (num == 4) {
+        zeldaimg.setAttribute ( "src", "img/zelda4.jpg")
+    } 
+    else if (num == 5) {
+        zeldaimg.setAttribute ( "src", "img/zelda5.jpg")
+    } 
+    else if (num == 6) {
+        zeldaimg.setAttribute ( "src", "img/zelda6.jpg")
+    } 
+    else if (num == 7) {
+        zeldaimg.setAttribute ( "src", "img/zelda7.jpg")
+    } 
+    else if (num == 8) {
+        zeldaimg.setAttribute ( "src", "img/zelda8.jpg")
+    } 
+    else if (num == 9) {
+        zeldaimg.setAttribute ( "src", "img/zelda9.jpg")
+    }
+    else if (num == 10) {
+        zeldaimg.setAttribute ( "src", "img/dowload.jpg")
+    }
+}
