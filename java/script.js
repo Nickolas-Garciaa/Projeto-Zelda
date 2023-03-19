941a6844a721e5ba8628cbfbd775abec98567659
Nickolas-Garciaa <nickolas.avelino@etec.sp.gov.br>
2023-03-19 13:51:49 -0300

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,38 +1,5 @@
-Function zelda (){
-    let zeldaimg = document.querySelector("img#imagem")
-    let num = math.trunc("math.random"()*10) ;
-
-    if (num == 0) {
-        zeldaimg.setAttribute ( "src", "img/zelda.jpg")
-    }
-    else if (num == 1) {
-        zeldaimg.setAttribute ( "src", "img/zelda1.jpg")
-    }
-    else if (num == 2) {
-        zeldaimg.setAttribute ( "src", "img/zelda2.jpg")
-    } 
-    else if (num == 3) {
-        zeldaimg.setAttribute ( "src", "img/zelda3.jpg")
-    } 
-    else if (num == 4) {
-        zeldaimg.setAttribute ( "src", "img/zelda4.jpg")
-    } 
-    else if (num == 5) {
-        zeldaimg.setAttribute ( "src", "img/zelda5.jpg")
-    } 
-    else if (num == 6) {
-        zeldaimg.setAttribute ( "src", "img/zelda6.jpg")
-    } 
-    else if (num == 7) {
-        zeldaimg.setAttribute ( "src", "img/zelda7.jpg")
-    } 
-    else if (num == 8) {
-        zeldaimg.setAttribute ( "src", "img/zelda8.jpg")
-    } 
-    else if (num == 9) {
-        zeldaimg.setAttribute ( "src", "img/zelda9.jpg")
-    }
-    else if (num == 10) {
-        zeldaimg.setAttribute ( "src", "img/dowload.jpg")
-    }
+function zelda (){
+    let imagem = document.getElementById("imagem");
+    let trocar = Math.trunc(Math.random()*10);
+    imagem.src = "img/zelda" + trocar + ".jpg";
 }
